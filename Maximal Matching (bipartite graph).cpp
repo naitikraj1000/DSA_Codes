ec7a2b9e27262a65c697290a73602d876811cdda
Naitik Raj <95161534+naitikraj1000@users.noreply.github.com>
2025-01-27 14:32:44 +0530

    Create Maximal Matching (bipartite graph).cpp

---
+++ Maximal Matching (bipartite graph).cpp
@@ -0,0 +1,8 @@
+// König's theorem states that the maximum matching number and the minimum vertex cover number are the same in a bipartite graph.
+// and in every graph, independent subsets and vertex covers are complements of each other (if S
+//  is a vertex cover, then the set of all vertices not belonging to S
+//  is an independent subset, and vice versa). So, the maximum independent subset is the complement of the minimum vertex cover.
+
+
+
+// Maximal Matching in bipartite graph (Alogrithm)
